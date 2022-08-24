72c9dd68276687028f8b87b327e1528d4aac3da2
Kevin Bell <bellKevin@pm.me>
2022-08-24 13:18:01 -0600

    Update Palindrome.java

--- palindrome/src/palindrome/Palindrome.java
+++ palindrome/src/palindrome/Palindrome.java
@@ -20,11 +20,11 @@ public class Palindrome {
             return true;
             //condition to verify that the first letter is different from the last letter
         } else if (phrase.charAt(0) != phrase.charAt(phrase.length() - 1)) {
-           // System.out.println(phrase.charAt(0) != phrase.charAt(phrase.length() - 1));
+            // System.out.println(phrase.charAt(0) != phrase.charAt(phrase.length() - 1));
             return false;
             //if it is the same, the function is called again by comparing the next letter at the beginning and end of the string
         } else {
-           // System.out.println(isPalindrome(phrase.substring(1, phrase.length() - 1)));
+            // System.out.println(isPalindrome(phrase.substring(1, phrase.length() - 1)));
             return isPalindrome(phrase.substring(1, phrase.length() - 1));
         } // end if/else condition
     } // end isPalindrome method
@@ -33,20 +33,22 @@ public class Palindrome {
         //Take input from the user
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Module 1 Competency Palindrome by Kevin Bell \n");
-        System.out.print("Enter a phrase: ");
+        System.out.printf("%16s", "Enter a phrase: ");
         String str = computerKeyboardInput.nextLine();   //Input the string
         //Check whether palindrome or not
         if (isPalindrome(str)) {
             //print the clean phrase
-            System.out.println("Clean phrase: " + str);
-            //print reverse phrase
-            System.out.println("Reverse phrase: "+str);
+            System.out.printf("%16s", "Clean phrase: ");
+            System.out.println(str);
+            System.out.printf("%16s", "Reverse phrase: ");
+            System.out.println(str);
             System.out.println("This IS a palindrome");
         } else {
             //print the clean phrase
-            System.out.println("Clean phrase: " + str);
-            //print reverse phrase
-            System.out.println("Reverse phrase: "+str);
+            System.out.printf("%16s", "Clean phrase: ");
+            System.out.println(str);
+            System.out.printf("%16s", "Reverse phrase: ");
+            System.out.println(str);
             System.out.println("This is NOT a palindrome");
         } // end if/else condition
     } // end main method
